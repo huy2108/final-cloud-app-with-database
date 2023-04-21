07a6b44481e0adef951e734463e2cb4d67b7a2f1
huy2108 <109428960+huy2108@users.noreply.github.com>
2023-04-21 22:17:01 +0700

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,24 +1,24 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner, Question, Choice
+#  Import any new Models here
+from .models import Course, Lesson, Instructor, Learner , Question, Choice 
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+# Register QuestionInline and ChoiceInline classes here
 
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
-class QuestionInLine(admin.StackedInline):
-    model = Question
-    extra = 3
-
-class ChoiceInLine(admin.StackedInline):
+class ChoiceInline(admin.StackedInline):
     model = Choice
-    extra = 3
+    extra = 4
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline,QuestionInLine]
+    inlines = [LessonInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
@@ -27,17 +27,10 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInLine]
-    list_display = ['text','mark']
-
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ['text','boo']
-# <HINT> Register Question and Choice models here
-
-admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice, ChoiceAdmin)
+# Register Question and Choice models here
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question)
+admin.site.register(Choice)
